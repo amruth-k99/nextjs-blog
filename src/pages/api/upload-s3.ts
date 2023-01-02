83df7703d0f3b9e7213b9fd5b7c5b644ca10c711
amruth-kuntamalla <88645369+amruth-kuntamalla@users.noreply.github.com>
2023-01-02 18:31:01 +0530

    removed empty default strings

--- src/pages/api/upload-s3.ts
+++ src/pages/api/upload-s3.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import S3 from "aws-sdk/clients/s3";
 
 const s3 = new S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS,
   region: process.env.AWS_S3_REGION,
 });
